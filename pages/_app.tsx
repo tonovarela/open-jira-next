c481ebaac22514a036dd216debe13cf55f4f9515
tonovarela <tonovarela@live.com>
2023-03-23 20:45:29 -0600

    Proyecto base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
+
+
+import { createTheme,CssBaseline,ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { lightTheme,darkTheme } from '../themes';
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+      <Component {...pageProps} />   
+      </CssBaseline>     
+    </ThemeProvider>
+  
+  )
 }
 
 export default MyApp
